e971fb945c0d8907fd97b55aa8cac7c067ce47ac
Garrett Truong <aznkungfubunnee@gmail.com>
2022-08-25 12:09:05 -0500

    heroku push

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 const cors = require('cors')
 require("dotenv").config()
-
+const path = require('path')
 // Setup your Middleware and API Router here
 const morgan = require('morgan')
 
@@ -20,8 +20,15 @@ app.use((req, res, next) => {
     next();
   });
 
-const router = require('./api')
+const router = require('./server/api')
 app.use('/api', router)
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.use((req, res, next) => {
+  console.log('hi')
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.get('*', (req, res) => {
   res.status(404).send({error: '404 - Not Found', message: 'No route found for the requested URL'});
@@ -33,8 +40,5 @@ app.use((error, req, res, next) => {
   res.send({error: error.message, name: error.name, message: error.message});
 });
 
-app.listen(3001,async () => {
-  console.log("Server is listening on 3001")
-} )
 
 module.exports = app;
